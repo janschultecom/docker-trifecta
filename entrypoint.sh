51eea889120d59548b8a91ab9b4efb5a04145513
Mathias Kub <git@makubi.at>
2016-11-21 01:30:10 +0100

    Exit if ZK_HOST env var is missing and do not create a pid file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,13 @@
 TRIFECTA_CONFIG="$1"
 
 # Setup trifecta configuration
-if [ -z ${ZK_HOST+x} ];
-then
-  echo "Zookeeper host not set, using localhost";
-  sed -i -e s/{{ZK_HOST}}/localhost/ "$TRIFECTA_CONFIG"
-else
-  echo "Using Zookeeper host: '$ZK_HOST'";
-  sed -i -e s/{{ZK_HOST}}/${ZK_HOST}/ "$TRIFECTA_CONFIG"
+if [ -z "${ZK_HOST}" ]; then
+  echo "Please set ZK_HOST env variable"
+  exit 1
 fi
 
+echo "Using Zookeeper host: '$ZK_HOST'";
+sed -i -e s/{{ZK_HOST}}/${ZK_HOST}/ "$TRIFECTA_CONFIG"
+
 # Start ui
-/home/trifecta/trifecta_ui/bin/trifecta_ui
+/home/trifecta/trifecta_ui/bin/trifecta_ui -Dpidfile.path=/dev/null
